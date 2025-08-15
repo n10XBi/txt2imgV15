7af216a3d1420c2120f41451a7f73495e6cc9574
n10XBi <164579514+n10XBi@users.noreply.github.com>
2025-08-15 12:46:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
     });
 
     // index.js
-    addEventListener("fetch", (event) => {
-        event.respondWith(handleRequest(event.request, event.env));
-    });
+    export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, env);
+  }
+}
+
 
     var ARTING_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo4OTE2MSwiZXhwIjoxNzU1MzYxMzQwfQ.WxECSI-XMa3vsQ9J4rtzsXV-LS1J035PnCyBH5be7jk";
     var BOT_TOKENS = /* @__PURE__ */ new Set();
